9b4b486bc2559a9a8943c8cb2f49d63a759e380f
Almeda2019 <111157642+Almeda2019@users.noreply.github.com>
2022-08-22 01:35:59 -0300

    Commit de  prueba

---
+++ src/main/java/ar/edu/unju/fi/tp0/Ejercicio1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ar.edu.unju.fi.tp0;
+
+/**
+ * @author Usuario
+ *
+ */
+public class Ejercicio1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola  Mundo");
+
+	}
+
+}
