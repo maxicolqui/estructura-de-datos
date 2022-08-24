b46b3dad55f75e5990e6373bb1ac844c619a66de
Maxi Colqui <108244836+maxicolqui@users.noreply.github.com>
2022-08-24 00:52:12 -0300

    Update Ejercicio2.java
    hola mundo

--- src/main/java/ar/edu/unju/fi/tp0/Ejercicio2.java
+++ src/main/java/ar/edu/unju/fi/tp0/Ejercicio2.java
@@ -1,10 +1,14 @@
 package ar.edu.unju.fi.tp0;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
+		Scanner sc = new Scanner(System.in);
+		
 
 	}
 
